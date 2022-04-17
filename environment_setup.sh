450577a51ef7a80221733c8a73dc3099263c8b07
Stephan <stephan.semerad@gmail.com>
2022-04-17 21:05:01 +0200

    set up

---
+++ environment_setup.sh
@@ -0,0 +1,8 @@
+python -m venv venv
+venv\scripts\activate
+
+
+pip install  matplotlib
+pip install  pandas
+pip install  pylab
+pip install  numpy
---
+++ linear_regression.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+
+
+df = pd.read_csv("FuelConsumption.csv")
+df.head()
